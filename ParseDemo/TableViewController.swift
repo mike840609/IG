8adf2ce5ff505e1842a8d53fc603fe85d6cdd550
mike840609 <mike840609@gmail.com>
2016-02-20 22:53:56 +0800

    Pull To Refresh

--- ParseDemo/TableViewController.swift
+++ ParseDemo/TableViewController.swift
@@ -14,62 +14,20 @@ class TableViewController: UITableViewController {
     var userids = [String]()
     var isFollowing = [String: Bool]()
     
+    var refresher : UIRefreshControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let query = PFUser.query()
+        refresher = UIRefreshControl()
+        refresher.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refresher.addTarget(self, action: "refresh", forControlEvents: .ValueChanged)
+        self.tableView.addSubview(refresher)
         
-        query?.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
-            
-            if let users = objects{
-                
-                self.usernames.removeAll(keepCapacity: true)
-                self.userids.removeAll(keepCapacity: true)
-                self.isFollowing.removeAll(keepCapacity:true)
-                
-                for object in users{
-                    
-                    if let user = object as? PFUser{
-                        
-                        if user.objectId != PFUser.currentUser()?.objectId{
-                            
-                            // 經由objectId判斷 可以避面將自身加入到陣列當中
-                            self.usernames.append(user.username!)
-                            self.userids.append(user.objectId!)
-                            
-                            let query = PFQuery(className: "followers")
-                            query.whereKey("follower", equalTo: (PFUser.currentUser()!.objectId)!)
-                            query.whereKey("following", equalTo: user.objectId!)
-                            
-                            query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
-                                
-                                if let objects = objects{
-                                    
-                                    if objects.count > 0{
-                                        
-                                        self.isFollowing[user.objectId!] = true
-                                        
-                                    }else{
-                                        
-                                        self.isFollowing[user.objectId!] = false
-                                    }
-                                }
-                                // 藉由此判斷讓 讓reloadData 只在最後一次執行
-                                if self.isFollowing.count == self.usernames.count{
-                                    self.tableView.reloadData()
-                                }
-                            })
-                        }
-                    }
-                }
-            }
-            
-            
-            
-        })
+        refresh()
+        
     }
-   
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -148,6 +106,62 @@ class TableViewController: UITableViewController {
             
             
         }
+        
+    }
+    
+    func refresh(){
+        
+        let query = PFUser.query()
+        
+        query?.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+            
+            if let users = objects{
+                
+                self.usernames.removeAll(keepCapacity: true)
+                self.userids.removeAll(keepCapacity: true)
+                self.isFollowing.removeAll(keepCapacity:true)
+                
+                for object in users{
+                    
+                    if let user = object as? PFUser{
+                        
+                        if user.objectId != PFUser.currentUser()?.objectId{
+                            
+                            // 經由objectId判斷 可以避面將自身加入到陣列當中
+                            self.usernames.append(user.username!)
+                            self.userids.append(user.objectId!)
+                            
+                            let query = PFQuery(className: "followers")
+                            query.whereKey("follower", equalTo: (PFUser.currentUser()!.objectId)!)
+                            query.whereKey("following", equalTo: user.objectId!)
+                            
+                            query.findObjectsInBackgroundWithBlock({ (objects, error) -> Void in
+                                
+                                if let objects = objects{
+                                    
+                                    if objects.count > 0{
+                                        
+                                        self.isFollowing[user.objectId!] = true
+                                        
+                                    }else{
+                                        
+                                        self.isFollowing[user.objectId!] = false
+                                    }
+                                }
+                                // 藉由此判斷讓 讓reloadData 只在最後一次執行
+                                if self.isFollowing.count == self.usernames.count{
+                                    self.tableView.reloadData()
+                                    self.refresher.endRefreshing()
+                                }
+                            })
+                        }
+                    }
+                }
+            }
+            
+            
+            
+        })
         
     }
     
